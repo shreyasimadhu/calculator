64219b58b5b32f4fe61d76fa61c153bd65ce163a
Shreyasi Madhu Shampa <91167803+shreyasimadhu@users.noreply.github.com>
2023-12-09 00:14:31 +0600

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+// For Back Button
+function backspace(){
+    var backspace = document.cform.calculatorDisplay.value
+    document.cform.calculatorDisplay.value = backspace.substring(0, backspace.length - 1);
+}
